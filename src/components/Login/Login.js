587323909b452d70b1a7ff12bfa259231fc53ec1
Curtis Hamm <curt.hamm@gmail.com>
2024-12-05 13:45:21 -0600

    add log in button

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,12 +12,12 @@ const Login = () => (
       </div>
       <div class="item welcome">
       </div>
-      <form className='flex flex-col mx-auto max-w-[80%]'>
+      <form className='flex flex-col mx-auto max-w-[70%]'>
         <h1 className='pl-4 font-medium text-4xl pb-4'>Welcome to Invest Safe</h1>
         <input type='text' className='border border-gray-400 mb-4 px-5 py-3 rounded' id='User ID' />
         <input type='text' className='border border-gray-400 px-5 py-3 rounded' id='Password' />
+        <button className='bg-[#0076a0] rounded mt-8 py-4 text-white'>Log In</button>
       </form>
-      <div>Item 3</div>
     </div>
     <div class="right">Right Content</div>
   </div>
