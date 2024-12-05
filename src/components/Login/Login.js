f8d37fbc4905a712955b15936e213d27c3eadb94
Curtis Hamm <curt.hamm@gmail.com>
2024-12-05 14:27:18 -0600

    add password mask and placeholders

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -14,12 +14,12 @@ const Login = () => (
       </div>
       <form className='flex flex-col mx-auto max-w-[70%]'>
         <h1 className='pl-4 font-medium text-4xl pb-4'>Welcome to Invest Safe</h1>
-        <input type='text' className='border border-gray-400 mb-4 px-5 py-3 rounded' id='User ID' />
-        <input type='text' className='border border-gray-400 px-5 py-3 rounded' id='Password' />
+        <input type='text' className='border border-gray-400 mb-4 px-5 py-3 rounded' placeholder='Username' id='User ID' />
+        <input type='password' className='border border-gray-400 px-5 py-3 rounded' placeholder='Password' id='Password' />
         <button className='bg-[#0076a0] rounded mt-8 py-4 text-white'>Log In</button>
       </form>
     </div>
-    <div class="right" className='max-h-full border-l-4 border-solid border-[#ffc107] bg-gray-800 '>
+    <div class="right" className='h-screen max-h-full border-l-4 border-solid border-[#ffc107] bg-gray-800 '>
       <div className='mt-20 text-4xl text-blue-900'>
         Right Content
       </div>
