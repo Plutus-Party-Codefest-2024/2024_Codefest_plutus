9f37bb620dfed047c7af289f53370eeed06ecee7
Curtis Hamm <curt.hamm@gmail.com>
2024-12-05 21:33:44 -0600

    Updated button targets in handleClicks

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,11 +6,24 @@ import './Landing.css'
 const Login = () => {
   const navigate = useNavigate(); // Hook for navigation
 
-  const handleLoginClient1 = (e) => {
+  const handleClickClient1 = (e) => {
     e.preventDefault();
-    navigate('/holdings1'); // Redirect to the Trade component
+    navigate('/holdings1');
   };
 
+  const handleClickClient2 = (e) => {
+    e.preventDefault();
+    navigate('/holdings2');
+  };
+  const handleClickClient3 = (e) => {
+    e.preventDefault();
+    navigate('/holdings3');
+  };
+
+  const handleClickFa = (e) => {
+    e.preventDefault();
+    navigate('/holdings3');
+  };
   return (
       <div className='mx-auto'>
         <div className='max-w-screen-md mx-auto'>
@@ -23,10 +36,10 @@ const Login = () => {
           </div>
           <form className='flex flex-col mx-auto max-w-[70%]'>
             <h1 className='pl-4 font-medium text-4xl pb-4'>Welcome to InvestSafe</h1>
-            <button className='bg-[#0076a0] rounded mt-8 py-4 text-white' onClick={handleLoginClient1}>Client 1 - Jenny</button>
-            <button className='bg-[#0076a0] rounded mt-8 py-4 text-white' onClick={handleLoginClient1}>Client 2 - John</button>
-            <button className='bg-[#0076a0] rounded mt-8 py-4 text-white' onClick={handleLoginClient1}>Client 3 - Brad</button>
-            <button className='bg-[#0076a0] rounded mt-8 py-4 text-white' onClick={handleLoginClient1}>Financial Advisor</button>
+            <button className='bg-[#0076a0] rounded mt-8 py-4 text-white' onClick={handleClickClient1}>Client 1 - Jenny</button>
+            <button className='bg-[#0076a0] rounded mt-8 py-4 text-white' onClick={handleClickClient2}>Client 2 - John</button>
+            <button className='bg-[#0076a0] rounded mt-8 py-4 text-white' onClick={handleClickClient3}>Client 3 - Brad</button>
+            <button className='bg-[#0076a0] rounded mt-8 py-4 text-white' onClick={handleClickFa}>Financial Advisor</button>
           </form>
           <div class="item welcome"></div>
         </div>
