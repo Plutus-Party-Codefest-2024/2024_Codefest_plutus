9ea344f8602fb3197aa1f077bb19cb7b9bbbafd0
Curtis Hamm <curt.hamm@gmail.com>
2024-12-05 21:11:19 -0600

    order1 route

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
 // import Holdings1 from './components/client1-holdings/Holdings'; 
 // import Holdings2 from './components/client2-holdings/Holdings'; 
-// import Holdings3 from './components/client3-holdings/Holdings'; 
+// import Holdings3 from './components/client3-holdings/Holdings';
+import Order1 from './components/client1-order/Order1';
 import Message from './components/Message/Message'; 
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Landing />} />
+      <Route path="/order1" element={<Order1 />} />
       {/* <Route path="/holdings1" element={<Holdings1 />} />
       <Route path="/holdings2" element={<Holdings2 />} />
       <Route path="/holdings3" element={<Holdings3 />} /> */}
