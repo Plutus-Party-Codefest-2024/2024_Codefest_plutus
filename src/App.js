ffd2e440e77aa91a7419f927bce6238bb114872c
unknown <humphries.cullen@gmail.com>
2024-12-05 21:02:42 -0600

    adding initial holding pages; update routes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login';
-import Trade from './components/Trades/Trades'; 
-import Message from './components/Message/Message'; 
+import Trade from './components/Trades/Trades';
+import Message from './components/Message/Message';
+import Holdings1 from './components/client1-holdings/holdings1';
+import Holdings2 from './components/client2-holdings/holdings2';
+import Holdings3 from './components/client3-holdings/holdings3';
 
 const App = () => (
   <Router>
@@ -11,6 +14,9 @@ const App = () => (
       <Route path="/" element={<Login />} />
       <Route path="/trade" element={<Trade />} />
       <Route path="/message" element={<Message />} />
+      <Route path="/holdings1" element={<Holdings1 />} />
+      <Route path="/holdings2" element={<Holdings2 />} />
+      <Route path="/holdings3" element={<Holdings3 />} />
     </Routes>
   </Router>
 );
