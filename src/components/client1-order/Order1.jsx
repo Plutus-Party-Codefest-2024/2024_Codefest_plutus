e5d5289abcfdfc1a26a674c612585fd1048a9862
Curtis Hamm <curt.hamm@gmail.com>
2024-12-05 23:33:15 -0600

    route and image

--- src/components/client1-order/Order1.jsx
+++ src/components/client1-order/Order1.jsx
@@ -17,17 +17,15 @@ function Order1() {
 
     return (
         <div>
-            <header className="bg-gray-700 text-white p-4">
-                <div className="flex items-center justify-between">
-                    <div className="text-xl font-bold">
-                        <img src="https://exitmap.com/wp-content/uploads/2022/03/Edward-Jones-logo.png" alt="Logo" class="h-8 inline-block mr-3" /> Online Trading
+            <header className='bg-gray-700 text-white p-4'>
+                <div className='flex items-center justify-between'>
+                    <div className='text-xl font-bold'>
+                        <img src='https://exitmap.com/wp-content/uploads/2022/03/Edward-Jones-logo.png' alt='Logo' class='h-8 inline-block mr-3' /> Online Access - Client Jenny Holdings
                     </div>
                     <nav>
-                        <ul className="flex space-x-6">
-                            <li><a href="/Login" className="hover:text-gray-200">Home</a></li>
-                            <li><a href='/Login' className="hover:text-gray-200">Secure Message</a></li>
-                            <li><a href="/Login" className="hover:text-gray-200">Contact</a></li>
-                            <li><a href="/Login" className="hover:text-gray-200">Trade</a></li>
+                        <ul className='flex space-x-6'>
+                            <li><a href='/' className='hover:text-gray-200'>Home Page</a></li>
+                            <li><a href='/order1' className='hover:text-gray-200'>Trade</a></li>
                         </ul>
                     </nav>
                 </div>
